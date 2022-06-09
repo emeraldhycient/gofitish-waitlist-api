4ad0734fb7eb5a21a6e873fee854c0ab00ce4302
emeraldhycient <igwezehycient86@gmail.com>
2022-06-09 23:39:35 +0100

    [add] added nodemailer remains to add creden tials next

--- src/controllers/waitlist.js
+++ src/controllers/waitlist.js
@@ -1,8 +1,12 @@
+const nodemailer = require("nodemailer");
+
 const waitlistSchema = require("../models/waitlist");
 
 const postWaitlist = (req, res) => {
   if (!req.body.category || !req.body.email) {
-    res.status(400).json({ message: "Please provide category and email" });
+    return res
+      .status(400)
+      .json({ message: "Please provide category and email" });
   }
 
   const waitlist = new waitlistSchema({
@@ -12,9 +16,36 @@ const postWaitlist = (req, res) => {
 
   waitlist.save((err, waitlist) => {
     if (err) {
-      res.status(500).json({ message: "Something went wrong" });
+      return res.status(500).json({ message: "Something went wrong" });
     } else {
-      res.status(200).json({ message: "Waitlist added" });
+      /*let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "testAccount.user", // generated ethereal user
+          pass: "testAccount.pass", // generated ethereal password
+        },
+      });
+
+      // send mail with defined transport object
+      transporter
+        .sendMail({
+          from: '"Fred Foo 👻" <foo@example.com>', // sender address
+          to: "bar@example.com, baz@example.com", // list of receivers
+          subject: "yaaaay welcome to the waitlist 🎉🥂 ✔", // Subject line
+          html: "<b>Hello world?</b>", // html body
+        })
+        .then((info) => {
+          console.log(info);
+          return res.status(200).json({ message: "Waitlist added" });
+        })
+        .catch((error) => {
+          console.log(error);
+          res.status(500).json({ message: "Something went wrong" });
+        });
+        */
+      return res.status(200).json({ message: "Waitlist added" });
     }
   });
 };
@@ -22,7 +53,7 @@ const postWaitlist = (req, res) => {
 const getWaitlist = (req, res) => {
   waitlistSchema.find({}, (err, waitlist) => {
     if (err) {
-      res.status(500).json({ message: "Something went wrong" });
+      return res.status(500).json({ message: "Something went wrong" });
     } else {
       res.status(200).json(waitlist);
     }
